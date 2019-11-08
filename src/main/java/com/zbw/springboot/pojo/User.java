b09248ce26b3fe4d91389d256a119f6f4daa8d2d
zbw <zbwjava@163.com>
2019-11-08 10:21:43 +0800

    增加简单用户实体类(可进行修改)

---
+++ src/main/java/com/zbw/springboot/pojo/User.java
@@ -0,0 +1,25 @@
+package com.zbw.springboot.pojo;
+
+/**
+ * Created by 郑博文 on 2019/11/8.
+ */
+public class User {
+    private String userName;
+    private String passWord;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
